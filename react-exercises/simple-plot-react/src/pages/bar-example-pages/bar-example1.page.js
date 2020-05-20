d1bb6ffddbe844a9bb661675395cfea4cb761a60
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-05-20 23:25:48 +0530

    Add basic file/folder structure

---
+++ react-exercises/simple-plot-react/src/pages/bar-example-pages/bar-example1.page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BarExample1 = () => {
+  return <div>Line chart (example 1)</div>;
+};
+
+export default BarExample1;
---
+++ react-exercises/simple-plot-react/src/pages/line-example-pages/line-example1.page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LineExample1 = () => {
+  return <div>Line chart (example 1)</div>;
+};
+
+export default LineExample1;
