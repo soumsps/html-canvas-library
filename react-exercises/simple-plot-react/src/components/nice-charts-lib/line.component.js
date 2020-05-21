c7ff4307d7f593a058baa983442b3d20e405915c
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-05-22 02:13:05 +0530

    Add both axis segments with labeling

--- react-exercises/simple-plot-react/src/components/nice-charts-lib/line.component.js
+++ react-exercises/simple-plot-react/src/components/nice-charts-lib/line.component.js
@@ -16,18 +16,82 @@ const LineChart = ({ chartOptions }) => {
   };
 
   const drawAxisX = (context) => {
+    const gap = 60;
+    const start = { x: width - (width - gap), y: height - gap };
+    const end = { x: width - gap, y: height - gap };
+    const xAxisLength = Math.abs(start.x - end.x);
+    const xAxisPartsNeeded =
+      Math.abs(chartOptions.axisX.start - chartOptions.axisX.end) /
+        chartOptions.axisX.interval +
+      1;
+
+    console.log('x axis length: ', xAxisLength);
+    console.log('x axis parts needed', xAxisPartsNeeded);
     context.lineWidth = 1.0;
     context.beginPath();
-    context.moveTo(width - (width - 30), height - 30);
-    context.lineTo(width - 30, height - 30);
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
     context.stroke();
+
+    for (let i = 0; i < xAxisPartsNeeded; i++) {
+      const stepSize = xAxisLength / xAxisPartsNeeded;
+
+      const nextSegement = (i + 1) * stepSize;
+      context.lineWidth = 1.0;
+      context.beginPath();
+      context.moveTo(start.x + nextSegement, start.y);
+      context.lineTo(start.x + nextSegement, start.y + 8);
+      context.stroke();
+
+      let label = chartOptions.axisX.start + chartOptions.axisX.interval * i;
+      const textMeasure = context.measureText(label);
+      context.font = '10px sans-serif';
+      context.fillText(
+        label,
+        start.x + nextSegement - textMeasure.width / 2,
+        start.y + 20
+      );
+    }
   };
+
   const drawAxisY = (context) => {
+    const gap = 60;
+    const start = { x: width - (width - gap), y: height - gap };
+    const end = { x: width - (width - gap), y: height - (height - gap) };
+    const yAxisLength = Math.abs(start.y - end.y);
+    const yAxisPartsNeeded =
+      Math.abs(chartOptions.axisY.start - chartOptions.axisY.end) /
+        chartOptions.axisY.interval +
+      1;
+
+    console.log('y axis length: ', yAxisLength);
+    console.log('y axis parts needed', yAxisPartsNeeded);
+
     context.lineWidth = 1.0;
     context.beginPath();
-    context.moveTo(width - (width - 30), height - 30);
-    context.lineTo(width - (width - 30), height - (height - 30));
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
     context.stroke();
+
+    for (let i = 0; i < yAxisPartsNeeded; i++) {
+      const stepSize = yAxisLength / yAxisPartsNeeded;
+
+      const nextSegement = i * stepSize;
+      context.lineWidth = 1.0;
+      context.beginPath();
+      context.moveTo(start.x, start.y - nextSegement);
+      context.lineTo(start.x - 8, start.y - nextSegement);
+      context.stroke();
+
+      let label = chartOptions.axisY.start + chartOptions.axisY.interval * i;
+      const textMeasure = context.measureText(label);
+      context.font = '10px sans-serif';
+      context.fillText(
+        label,
+        start.x - 15 - textMeasure.width,
+        start.y - nextSegement + 5
+      );
+    }
   };
 
   useEffect(() => {
