a9f77905b18bb1e362924a5896a4f32f8947b251
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-05-21 10:33:25 +0530

    Add canvas to example page This is just a sample, final product don't need to be like this.

--- react-exercises/simple-plot-react/src/pages/bar-example-pages/bar-example1.page.js
+++ react-exercises/simple-plot-react/src/pages/bar-example-pages/bar-example1.page.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const BarExample1 = () => {
+  const canvasRef = React.useRef(null);
   return (
     <div>
       <h3>Bar chart (example 1)</h3>
+      <canvas
+        className="canvas-bar"
+        ref={canvasRef}
+        width={600}
+        height={400}
+        onClick={(e) => {
+          alert(e.clientX);
+        }}
+      />
     </div>
   );
 };
--- react-exercises/simple-plot-react/src/pages/line-example-pages/line-example1.page.js
+++ react-exercises/simple-plot-react/src/pages/line-example-pages/line-example1.page.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const LineExample1 = () => {
+  const canvasRef = React.useRef(null);
   return (
     <div>
       <h3>Line chart (example 1)</h3>
+      <canvas
+        className="canvas-line"
+        ref={canvasRef}
+        width={600}
+        height={400}
+        onClick={(e) => {
+          alert(e.clientX);
+        }}
+      />
     </div>
   );
 };
