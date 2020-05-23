8b9e03a4b9bb55ac4443c397af5ae1d070f6505c
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-05-23 19:14:46 +0530

    update homepage with documentation link

--- react-exercises/simple-plot-react/src/pages/homepage/homepage.page.js
+++ react-exercises/simple-plot-react/src/pages/homepage/homepage.page.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <a
+        href="https://github.com/pesto-students/deep-dive-soumsps/tree/simple-plot-react/react-exercises/simple-plot-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Read Documentation
+      </a>
+    </div>
+  );
 };
 
 export default Homepage;
