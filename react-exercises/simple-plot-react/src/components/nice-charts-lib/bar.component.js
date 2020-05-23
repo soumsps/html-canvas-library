939eb07eb791bb594dfe4aa6ccefa87a4102b0ab
vikas <vikas18687@gmail.com>
2020-05-23 16:41:50 +0530

    add xAxis, yAxis through helper functions

--- react-exercises/simple-plot-react/src/components/nice-charts-lib/bar.component.js
+++ react-exercises/simple-plot-react/src/components/nice-charts-lib/bar.component.js
@@ -1,45 +1,11 @@
 import React, { useEffect, useRef } from 'react';
+import { drawAxisY, drawAxisX } from './helper';
 
 const drawSingleBar = (ctx, startX, startY, width, height, color) => {
   ctx.save();
   ctx.fillStyle = color;
   ctx.fillRect(startX, startY, width, height);
   ctx.restore();
-};
-
-const drawAxisX = (context, xAxisParams) => {
-  const { maxBarsInVariant, width, height, chartOptions } = xAxisParams;
-  const gap = 18;
-  const start = { x: width - (width - gap), y: height - gap };
-  const end = { x: width - gap, y: height - gap };
-  const xAxisLength = Math.abs(start.x - end.x);
-
-  const xAxisPartsNeeded = maxBarsInVariant;
-  context.lineWidth = 1.0;
-  context.beginPath();
-  context.moveTo(start.x, start.y);
-  context.lineTo(end.x, end.y);
-  context.stroke();
-
-  for (let i = 0; i < xAxisPartsNeeded; i++) {
-    const stepSize = xAxisLength / xAxisPartsNeeded;
-
-    const nextSegement = (i + 1) * stepSize;
-    context.lineWidth = 1.0;
-    context.beginPath();
-    context.moveTo(start.x + nextSegement, start.y);
-    context.lineTo(start.x + nextSegement, start.y + 8);
-    context.stroke();
-
-    let label = chartOptions.axisX.start + chartOptions.axisX.interval * i;
-    const textMeasure = context.measureText(label);
-    context.font = '10px sans-serif';
-    context.fillText(
-      label,
-      start.x + nextSegement - textMeasure.width / 2,
-      start.y + 20
-    );
-  }
 };
 
 const getMaxDataOfBar = (chartOptions) => {
@@ -66,24 +32,24 @@ const drawBarChart = (chartOptions, canvasRef) => {
   const canvas = canvasRef.current;
   const canvasOptions = {};
   canvasOptions.context = canvas.getContext('2d');
-  canvasOptions.padding = 20;
-  const options = canvasOptions;
+  canvasOptions.padding = 30;
   const chartData = chartOptions.data;
   const width = chartOptions.chartWidth;
   const height = chartOptions.chartHeight;
-  const canvasHeight = height - options.padding * 2;
-  const canvasWidth = width - options.padding * 2;
+  const canvasHeight = height - canvasOptions.padding * 2;
+  const canvasWidth = width - canvasOptions.padding * 2;
   const { maxBarsInVariant, maxHeight } = getMaxDataOfBar(chartOptions);
 
   const barVariant = chartData.length + 1;
   const barSize = canvasWidth / (maxBarsInVariant * barVariant);
   const colors = ['#52dedd', '#e0e438', '#f7885d', '#f1558e', '#f1558e'];
-  drawAxisX(options.context, {
+  drawAxisX(canvasOptions.context, {
     maxBarsInVariant,
     width,
     height,
     chartOptions,
   });
+  drawAxisY(canvasOptions.context, chartOptions, maxHeight);
   let barIndex = 0;
   for (let i = 0; i < maxBarsInVariant; i += 1) {
     let colorIndex = 0;
@@ -97,13 +63,13 @@ const drawBarChart = (chartOptions, canvasRef) => {
         const barHeight = Math.round(
           (canvasHeight / maxHeight) * chartData[j]['data'][i][1]
         );
-        const x = options.padding + barIndex * barSize;
-        const y = height - barHeight - options.padding;
+        const x = canvasOptions.padding + barIndex * barSize;
+        const y = height - barHeight - canvasOptions.padding;
         console.log(
           `height => ${barHeight}, width => ${barSize} x => ${x} y => ${y}`
         );
         drawSingleBar(
-          options.context,
+          canvasOptions.context,
           x,
           y,
           barSize,
@@ -124,7 +90,6 @@ const BarChart = ({ chartOptions }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    //const chartData = chartOptions.data;
     drawBarChart(chartOptions, canvasRef);
   });
 
